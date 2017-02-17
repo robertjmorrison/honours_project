5a5ff28a741756706b00e09f22226e29b4bf10d9
Robert Morrison <robertjmorrison1994@gmail.com>
2017-02-17 14:45:29 +0000

    Adding Initial Models and DB

--- project/models.py
+++ project/models.py
@@ -1,3 +1,27 @@
 from django.db import models
 
-# Create your models here.
+
+class Game(models.Model):
+    date_time = models.DateTimeField('Game Date')
+    opponent = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.date_time
+
+
+class Player(models.Model):
+    name = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.name
+
+
+class Play(models.Model):
+    game = models.ForeignKey(Game, on_delete=models.CASCADE)
+    qb = models.ForeignKey(Player, on_delete=models.CASCADE)
+    type = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.type
+
+
