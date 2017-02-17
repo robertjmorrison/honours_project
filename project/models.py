47028cf6265fc584b3030928dc52fa8c120fc354
Robert Morrison <robertjmorrison1994@gmail.com>
2017-02-17 21:03:24 +0000

    Added play and player abstract base classes

--- project/models.py
+++ project/models.py
@@ -29,35 +29,91 @@ class Player(models.Model):
         return "%s %s" % (self.first_name, self.second_name)
 
 
+class Quarterback(Player):
+    pass_rating = models.IntegerField
+
+    def __str__(self):
+        return "%s %s" % (self.first_name, self.second_name)
+
+
 class Play(models.Model):
-    # define play types here in human-readable constant
-    PASS = 'PA'
-    QB_RUN = 'QR'
-    HAND_OFF = 'HO'
-
-    PLAY_TYPES = (
-        (PASS, 'Pass'),
-        (QB_RUN, 'Quarterback Run'),
-        (HAND_OFF, 'Hand-Off')
-    )
-
-    game = models.ForeignKey(Game, on_delete=models.CASCADE)
-    qb = models.ForeignKey(Player, on_delete=models.CASCADE)
-    type = models.CharField(max_length=50, choices=PLAY_TYPES)
-
-    # play context: down, distance, field_half, yard_line, quarter, time, timeouts
-
-    down = models.IntegerField(validators=[MaxValueValidator(4), MinValueValidator(1)])
-    distance = models.IntegerField(validators=[MaxValueValidator(99), MinValueValidator(0.5)])
+    # field positions
     OWN = 'OWN'
     OPP = 'OPP'
 
+    # possible field halves
     FIELD_HALVES = (
         (OWN, 'Own'),
         (OPP, 'Opponent\'s')
     )
 
+    game = models.ForeignKey(Game, on_delete=models.CASCADE)
+
+    # play context: down, distance, field_half, yard_line, quarter, time, outcome, gain
+    down = models.IntegerField(validators=[MaxValueValidator(4), MinValueValidator(1)])
+    distance = models.IntegerField(validators=[MaxValueValidator(99), MinValueValidator(0.5)])
+    field_half = models.CharField(max_length=3, choices=FIELD_HALVES)
+    yard_line = models.IntegerField(validators=[MaxValueValidator(50), MinValueValidator(0.5)])
+    quarter = models.IntegerField(validators=[MaxValueValidator(4), MinValueValidator(1)])
+    time = models.TimeField()  # need to somehow make sure this displays as MM:SS
+    gain = models.IntegerField(validators=[MaxValueValidator(100)])
+
+    class Meta:
+        abstract = True
+
     def __str__(self):
         # e.g. (Q3 - 04:31) 3rd & 7, Pass, Gain of 14
         return "(Q%s, - %s) %s % %s, %s, Gain of %s" % (self.quarter, self.time, self.down, self.distance,
-                                                        self.type, self.outcome)
+                                                        self.type, self.outcome, self.gain)
+
+
+class PassPlay(Play):
+    INT = 'INT',
+    TD = 'TD'
+    PA = 'PA'
+
+    OUTCOMES = (
+        (TD, 'Touchdown'),
+        (INT, 'Interception'),
+        (PA, 'Pass Attempt')
+
+    )
+    complete = models.BooleanField
+    outcome = models.CharField(max_length=3, choices=OUTCOMES)
+    passer = models.ForeignKey(Player, on_delete=models.CASCADE, related_name='play_passer')
+    receiver = models.ForeignKey(Player, on_delete=models.CASCADE, related_name='play_receiver')
+
+    def __str__(self):
+        # example: Touchdown: QB #13 throws 17-Yard pass to #84
+        return "#%s throws %s-Yard %s pass to %s" % (self.outcome, self.passer.jersey_no, self.gain, self.receiver)
+
+
+class RunPlay(Play):
+    TD = 'TD',
+    FUM_REC = 'FR',
+    FUM_LOST = 'FL',
+    RUN = 'RN'
+
+    OUTCOMES = (
+        (TD, 'Touchdown'),
+        (FUM_REC, 'Fumbled, recovered'),
+        (FUM_LOST, 'Lost Fumble'),
+        (RUN, 'QB Rush')
+    )
+    outcome = models.CharField(max_length=3, choices=OUTCOMES)
+    runner = models.ForeignKey(Player, on_delete=models.CASCADE)
+
+    class Meta:
+        abstract = False
+
+    def __str__(self):
+        # example: Touchdown: QB #7 runs the ball for a gain of 8 yards
+        return "%s: QB #%s runs the ball for a gain of %s yards" % (self.outcome, self.runner.jersey_no, self.gain)
+
+
+class HandOffPlay(RunPlay):
+    quarterback = models.ForeignKey(Player, on_delete=models.CASCADE)
+
+    def __str__(self):
+        # example: Hand-off: RB #23 runs the ball for a gain of 9 yards
+        return "Hand-off: RB #%s runs the ball for a gain of %s" % (self.runner, self.gain)
