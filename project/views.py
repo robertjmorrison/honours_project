88ee585881c82a3351ce3083172a0f2bc316a67d
Robert Morrison <robertjmorrison1994@gmail.com>
2017-02-17 21:36:00 +0000

    Added in very basic functionality - needs to be converted into class based views

--- project/views.py
+++ project/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from django.views.generic import TemplateView
+from .models import *
 
-# Create your views here.
+
+def index(request):
+    game_list = Game.objects.order_by('date_time')
+
+    context = {
+        'game_list': game_list,
+    }
+
+    return render(request, 'project/index.html', context)
